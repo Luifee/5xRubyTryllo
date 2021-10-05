347054ca2ffc8109d6ed4af8570b852867dc3e72
luifee <lynnyuhuang@gmail.com>
2021-10-05 12:43:45 +0000

    3.10devise

--- app/javascript/stylesheets/index.js
+++ app/javascript/stylesheets/index.js
@@ -1,5 +1,5 @@
 import "tailwindcss/base";
 import "tailwindcss/components";
-import "./application";
+import "./application.scss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "tailwindcss/utilities";
